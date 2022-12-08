5e0c5abea079687517316c7097daa112efa5a2bc
satoshi-sh <satoss1108@gmail.com>
2022-12-08 16:02:16 -0600

    add Nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './components/header/Header'
+import TopBar from './components/topbar/TopBar'
 
 function App() {
   return (
     <div className="App">
+      <TopBar />
       <Header />
+
     </div>
   );
 }
