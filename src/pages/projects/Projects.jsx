cee89b3ecf19ac19b47e36e80266f66de5b65805
satoshi-sh <satoss1108@gmail.com>
2023-04-05 15:32:28 -0500

    Dropped a project from a page. Made a code cleaner with a ternary operator.

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -15,19 +15,21 @@ const Projects = ()=>{
     const descriptions = {
       'Take Stairs':"This application keeps track of user's stair exercises. Users just need to put how many stairs they climb up and down. It shows the stats by bar and line plots, visualized data with Recharts. API was created with express, interaction with MongoDB to get stored data. Passport.js was used to authorize the user.",
       'Memory Game2':'Memory Card game was build with React. Used React Router DOM to switch pages and React Context to change the design of the whole application (You can click on the card in the navigation bar to change). JavaScript classes was used to build trump card deck, to keep the code clearner.',
-      'Shopping Cart':'Shopping Cart was made by React. User can make a mock order on this application.',
       'Typing Game':'Typing game was made with React, hosted on GitHub page. Typewriter Effect was used to make typing animation in the start page. Users can choose sound (on/off) and difficulty level.',
-      'Slack Mock':'Used React and Firebase. Once account is created, the user can make a post on a specific topic thread.',
       'Todo List':'User can make to do list. Local storage is used to keep data. Javascript object was used to organized the code.',
-      'Wheres Waldo':"Where's Waldo game with Ghibli characters made with React. Firebase was used to keep the character locations and time score of users."
-      
+      'Django Portfolio':'Fullstack website created with Django, hosted on Pythonanywhere, and showcasing projects and blog posts. Used flexbox and bootstrap to design the page layouts, and Sqllite as a database. Blog posts can be searched by categories.'
     }
     const content = Object.entries(pros)
     .map(([key,value])=>
     {
       let link_name = key.split('.')[0].toLowerCase()
-      if (link_name==='slack_mock')
-      {link_name='slack-mock'} 
+      if (link_name==='slack_mock'|| link_name=='take_stairs')
+      {link_name= link_name.replace('_','-')} 
+      if (link_name ==='django_portfolio')
+      {link_name ='django-portofolio'} 
+      const gitLink = "https://github.com/Satoshi-Sh/" + link_name
+      const liveLink = link_name=='django-portofolio'? 'https://satoshi40.pythonanywhere.com/' : "https://satoshi-sh.github.io/"+link_name
+
       const name= key.split('.')[0].replace('_',' ')
       return (
         <div key={name} className='one-project'>
@@ -44,8 +46,8 @@ const Projects = ()=>{
             src={value} alt={name} className='project'>
             </motion.img>
             <div className="links" key={name}>
-                    <div className="link" key={name+'1'}><a href={"https://github.com/Satoshi-Sh/"+link_name} target="_blank"  rel="noreferrer">View Code</a></div>
-                    <div className="link" key={name+'2'}><a href={"https://satoshi-sh.github.io/"+link_name} target="_blank"  rel="noreferrer">Live Preview</a></div>
+                    <div className="link" key={name+'1'}><a href={gitLink} target="_blank"  rel="noreferrer">View Code</a></div>
+                    <div className="link" key={name+'2'}><a href={liveLink} target="_blank"  rel="noreferrer">Live Preview</a></div>
             </div>
           </motion.div>    
           <AnimatePresence>
@@ -63,7 +65,7 @@ const Projects = ()=>{
 
     return (<div className='projects'>
                <h1>Projects</h1>
-               <p>*Click pictures to see a summary of a project.</p>
+               <p>*Click a picture to see the summary of the project.</p>
                <div className='projects-div'>
                {content}
                </div>
